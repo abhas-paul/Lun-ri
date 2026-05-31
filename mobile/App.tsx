93178d773a665d4d03dd5f344e4b5fbf33d740e8
abhas-paul <apodul456@gmail.com>
2026-05-31 20:16:55 +0530

    Created HomeScreen Component.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { HomeScreen } from './components/index';
 import './global.css';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <View className="flex-1 items-center justify-center">
-        <StatusBar style="auto" />
-        <Text>Hello, World!</Text>
+      <View className="flex-1 items-center justify-center bg-[#151515]">
+        <StatusBar style="light" backgroundColor="black" />
+        <HomeScreen />
       </View>
     </SafeAreaProvider>
   );
