7823a105bc03d4215df122b587dede6965a6af0b
abhas-paul <apodul456@gmail.com>
2026-06-07 11:46:08 +0530

    Initial Project Upload

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/api/auth/signup", (req, res) => {
+    res.send("Signup Route");
+});
+
+app.get("/api/auth/login", (req, res) => {
+    res.send("login Route");
+});
+
+app.get("/api/auth/logout", (req, res) => {
+    res.send("logout Route");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: `, PORT)
+});
