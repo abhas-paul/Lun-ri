6f84cae97c4e5ae67fb868fafff27d4f507ce82e
abhas-paul <apodul456@gmail.com>
2026-06-09 12:35:05 +0530

    Add CORS support, Axios instance, and React Query integration

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from './routes/authRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -12,6 +13,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true // allow cookies from frontend.
+}));
 app.use(express.json());
 app.use(cookieParser());
 
