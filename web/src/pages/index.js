ffd094d546ed8728194a48fbfada89e1397bee54
abhas-paul <apodul456@gmail.com>
2026-06-09 12:06:20 +0530

    Setting up frontend before build.

---
+++ web/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from './Home.jsx';
+import Call from './Call.jsx';
+import Chat from './Chat.jsx';
+import Login from './Login.jsx';
+import Notifications from './Notifications.jsx';
+import OnBoarding from './OnBoarding.jsx';
+import SignUp from './SignUp.jsx';
+
+export {
+    Home,
+    Call,
+    Chat,
+    Login,
+    Notifications,
+    OnBoarding,
+    SignUp,
+};
